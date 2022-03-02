0ff8ba3ebcf217fb10dc0a6056d1fb120b2cc244
abhinavJai <jaisawal99@gmail.com>
2022-03-02 16:08:20 +0530

    3 files modified, 1 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+enum Operator {
+  ADD = 'ADD',
+  SUBTRACT = 'SUBTRACT',
+  MULTIPLY = 'MULTIPLY',
+  DIVIDE = 'DIVIDE',
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name = 'Angular';
+  previousNum: string = null;
+  currentNum = '0';
+  operator: Operator = null;
+  latestClickedNumber: number = null;
+  lastClickedButton: string = null;
+
+  click(n: number) {
+    this.latestClickedNumber = n;
+    if (this.lastClickedButton in Operator || this.currentNum === '0') {
+      this.currentNum = n.toString();
+    } else this.currentNum = this.currentNum.concat(n.toString());
+
+    this.lastClickedButton = n.toString();
+  }
+
+  /**
+   * Resets the calculator back to original state.
+   */
+  allClear(): void {
+    this.currentNum = '0';
+    this.previousNum = null;
+    this.operator = null;
+  }
 }
