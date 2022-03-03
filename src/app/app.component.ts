92d56fd15de9872cb77f998235d4d6df13b621c1
abhinavJai <jaisawal99@gmail.com>
2022-03-03 22:30:19 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,99 @@
 import { Component } from '@angular/core';
 
-enum Operator {
-  ADD = 'ADD',
-  SUBTRACT = 'SUBTRACT',
-  MULTIPLY = 'MULTIPLY',
-  DIVIDE = 'DIVIDE',
-}
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  previousNum: string = null;
+  previousNum: number = null;
   currentNum = '0';
-  operator: Operator = null;
   latestClickedNumber: number = null;
-  lastClickedButton: string = null;
+  clickedItem: string;
 
-  click(n: number) {
-    this.latestClickedNumber = n;
-    if (this.lastClickedButton in Operator || this.currentNum === '0') {
-      this.currentNum = n.toString();
-    } else this.currentNum = this.currentNum.concat(n.toString());
+  click(n: string) {
+    localStorage.setItem('dataSource', n);
+    // debugger;
 
-    this.lastClickedButton = n.toString();
+    switch (n) {
+      case '1':
+        if (document.getElementById('myButtonn1').innerText !== '0') {
+          document.getElementById('myButtonn1').innerText = '0';
+        } else {
+          document.getElementById('myButtonn1').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '2':
+        if (document.getElementById('myButtonn2').innerText !== '0') {
+          document.getElementById('myButtonn2').innerText = '0';
+        } else {
+          document.getElementById('myButtonn2').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+
+      case '3':
+        if (document.getElementById('myButtonn3').innerText !== '0') {
+          document.getElementById('myButtonn3').innerText = '0';
+        } else {
+          document.getElementById('myButtonn3').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '4':
+        if (document.getElementById('myButtonn4').innerText !== '0') {
+          document.getElementById('myButtonn4').innerText = '0';
+        } else {
+          document.getElementById('myButtonn4').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '5':
+        if (document.getElementById('myButtonn5').innerText !== '0') {
+          document.getElementById('myButtonn5').innerText = '0';
+        } else {
+          document.getElementById('myButtonn5').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '6':
+        if (document.getElementById('myButtonn6').innerText !== '0') {
+          document.getElementById('myButtonn6').innerText = '0';
+        } else {
+          document.getElementById('myButtonn6').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '7':
+        if (document.getElementById('myButtonn7').innerText !== '0') {
+          document.getElementById('myButtonn7').innerText = '0';
+        } else {
+          document.getElementById('myButtonn7').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '8':
+        if (document.getElementById('myButtonn8').innerText !== '0') {
+          document.getElementById('myButtonn8').innerText = '0';
+        } else {
+          document.getElementById('myButtonn8').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+      case '9':
+        if (document.getElementById('myButtonn9').innerText !== '0') {
+          document.getElementById('myButtonn9').innerText = '0';
+        } else {
+          document.getElementById('myButtonn9').innerText =
+            localStorage.getItem('dataSource');
+        }
+        break;
+    }
   }
 
-  /**
-   * Resets the calculator back to original state.
-   */
   allClear(): void {
     this.currentNum = '0';
     this.previousNum = null;
-    this.operator = null;
   }
 }
